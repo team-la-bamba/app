05587c9180b5e3d5b0b753d8885a19978db14c73
Marcus Tisäter <marcus.tisater@nordicmorning.com>
2020-04-05 00:18:46 +0200

    Fixes padding

--- src/components/Question.js
+++ src/components/Question.js
@@ -19,7 +19,7 @@ function Question({ answers, onSubmit = () => {}, text, type }) {
 
   return (
     <div className="formContainer bg-white mt-10 sm:mx-auto sm:w-full sm:max-w-md">
-      <div className="questionContainer bg-white py-16 px-4 shadow sm:rounded-lg sm:px-24">
+      <div className="questionContainer bg-white py-14 px-4 shadow sm:rounded-lg sm:px-24">
         <h2 className="text-2xl leading-8 my-8 font-semibold font-display text-gray-900 sm:text-3xl sm:leading-9 mt-0">
           {text}
         </h2>
