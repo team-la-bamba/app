545923cede23f10fb0a1a5c75faa9192f0cc9f6b
Eleonor Dammfors <eleonor.dammfors@gmail.com>
2020-04-04 21:03:22 +0200

    merging

--- src/components/Select.js
+++ src/components/Select.js
@@ -3,6 +3,33 @@ import Select from 'react-select';
 
 export default ({ label, onChange, options, placeholder, value }) => {
   const selected = options.filter((o) => o.value === value).pop();
+  
+  const customStyles = {
+    option: (provided) => ({
+      ...provided,
+      padding: 20,
+      color: "#333"
+    }),
+    control: (provided) => ({
+        ...provided,
+        borderColor: "#9fa6b2",
+        padding: "0"
+    }),
+    valueContainer: (provided) => ({
+        ...provided,
+        padding: "0"
+    }),
+    indicatorSeparator: () => ({}),
+    dropdownIndicator: (provided) => ({
+        ...provided,
+        color: '#333'
+    }),
+    placeholder: () => ({
+        color: '#333',
+        paddingLeft: '1rem'
+    })
+  } 
+
   return (
     <Select
       aria-label={label}
@@ -12,6 +39,7 @@ export default ({ label, onChange, options, placeholder, value }) => {
       isSearchable={true}
       isClearable={true}
       options={options}
+      styles={customStyles}
     />
   );
 };
