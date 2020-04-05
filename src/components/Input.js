75a80dd5ac1c94f9fabc442949feb672f1266105
Eleonor Dammfors <eleonor.dammfors@gmail.com>
2020-04-05 12:40:00 +0200

    style question buttons

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,36 +1,39 @@
-import React from 'react';
+import React from 'react'
 
-function Input({ _id, onClick, selected, text }) {
+function Input ({ _id, onClick, selected, text }) {
   const fakeEvent = {
     persist: () => {},
     target: {
       name: 'answer',
-      value: _id,
-    },
-  };
+      value: _id
+    }
+  }
 
   return (
     <div
-      className="flex content-center border rounded-lg border-black border-2 py-4 .my-3 my-3"
+      className='flex content-center w-64 max-w-5xl border-2 rounded-lg border-blue-300 py-4 .my-3 my-3'
       onClick={() => onClick(fakeEvent)}
-      style={{ backgroundColor: selected ? '#f5f2ed' : '' }}
+      style={{
+        backgroundColor: selected ? '#EBF8FF' : '',
+        borderColor: selected ? '#63B3ED' : ''
+      }}
     >
-      <div className="flex ml-3 items-start">
+      <div className='flex ml-3 items-start'>
         <input
           onChange={onClick}
           id={fakeEvent.target.name}
           name={fakeEvent.target.name}
-          type="radio"
+          type='radio'
           value={fakeEvent.target.value}
           checked={selected}
-          className="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out __web-inspector-hide-shortcut__"
+          className='form-radio h-4 w-4 transition duration-150 ease-in-out __web-inspector-hide-shortcut__'
         />
-        <label htmlFor={fakeEvent.target.name} className="-mt-1 ml-3">
+        <label htmlFor={fakeEvent.target.name} className='-mt-1 ml-3'>
           {text}
         </label>
       </div>
     </div>
-  );
+  )
 }
 
-export default Input;
+export default Input
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,26 +1,26 @@
-import React from "react";
-import Input from "./Input";
-import Button from "./Button";
-import Error from "./Error";
-import { useForm } from "../hooks";
+import React from 'react'
+import Input from './Input'
+import Button from './Button'
+import Error from './Error'
+import { useForm } from '../hooks'
 
-function Question({ answers, onSubmit = () => {}, text, type }) {
+function Question ({ answers, onSubmit = () => {}, text, type }) {
   const { values, handleChange, handleSubmit, errors } = useForm(
     onSubmit,
     values => {
       if (!Object.keys(values).length) {
         return {
-          error: "Inget svar är valt",
-        };
+          error: 'Inget svar är valt'
+        }
       }
-      return {};
+      return {}
     }
-  );
+  )
 
   return (
-    <div className="formContainer bg-white mt-10 sm:mx-auto sm:w-full sm:max-w-md">
-      <div className="questionContainer bg-white py-14 px-4 shadow sm:rounded-lg sm:px-24">
-        <h2 className="text-2xl leading-8 my-8 font-semibold font-display text-gray-900 sm:text-3xl sm:leading-9 mt-0">
+    <div className='formContainer bg-white mt-10 sm:mx-auto sm:w-full sm:max-w-md'>
+      <div className='questionContainer bg-white py-14 px-4 shadow sm:rounded-lg sm:px-24'>
+        <h2 className='text-2xl leading-8 my-8 font-semibold font-display text-gray-900 sm:text-3xl sm:leading-9 mt-0'>
           {text}
         </h2>
 
@@ -41,7 +41,7 @@ function Question({ answers, onSubmit = () => {}, text, type }) {
         </form>
       </div>
     </div>
-  );
+  )
 }
 
-export default Question;
+export default Question
