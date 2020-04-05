9b82c1ef8cf5612e3540ddb22f2cbdddade0b275
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-04-05 14:40:20 +0200

    Add cookie rember for place

--- src/App.js
+++ src/App.js
@@ -1,57 +1,74 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
-import './tailwind-ui.min.css'
-import Question from './components/Question'
-import Select from './components/Select'
-import Done from './components/Done'
-import Button from './components/Button'
-import Header from './components/Header'
-import Error from './components/Error'
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import './tailwind-ui.min.css';
+import Question from './components/Question';
+import Select from './components/Select';
+import Done from './components/Done';
+import Button from './components/Button';
+import Header from './components/Header';
+import Error from './components/Error';
+import { useCookies } from 'react-cookie';
 
-const url = 'https://api.labamba.space'
+//const url = 'https://api.labamba.space'
+const url = 'http://localhost:3000';
 
-function App () {
-  const [answers, setAnswers] = useState([])
-  const [place, setPlace] = useState('')
-  const [questionIndex, setQuestionIndex] = useState(0)
-  const [done, setDone] = useState(false)
-  const [questions, setQuestions] = useState([])
-  const [places, setPlaces] = useState([])
-  const [response, setResponse] = useState(null)
-  const [error, setError] = useState('')
+function App() {
+  const [cookies, setCookie, removeCookie] = useCookies(['place']);
+  const [answers, setAnswers] = useState([]);
+  const [place, setPlace] = useState(cookies.place);
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [done, setDone] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [places, setPlaces] = useState([]);
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState('');
 
-  useEffect(() => {
-    fetch(url + '/places')
-      .then(res => res.json())
-      .then(body => setPlaces(body))
-  }, [])
+  const onCookieChange = (place) => {
+    setCookie('place', place, { path: '/', expires: new Date(2147483647 * 1000) });
+  };
+
+  const onChangePlaceClick = (e) => {
+    e.preventDefault();
+    removeCookie('place');
+    setPlace('');
+  };
 
   const fetchQuestions = () => {
     fetch(url + '/questions')
-      .then(res => res.json())
-      .then(body => setQuestions(body))
-  }
+      .then((res) => res.json())
+      .then((body) => setQuestions(body));
+  };
 
-  const submitAnswers = answers => {
+  useEffect(() => {
+    fetch(url + '/places')
+      .then((res) => res.json())
+      .then((body) => setPlaces(body));
+
+    if (place) {
+      fetchQuestions();
+    }
+  }, [place]);
+
+  const submitAnswers = (answers) => {
     fetch(url + '/answers', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(answers)
+      body: JSON.stringify(answers),
     })
-      .then(res => res.json())
-      .then(body => {
-        setError('')
-        setResponse(body)
-        setDone(true)
-      })
-  }
+      .then((res) => res.json())
+      .then((body) => {
+        setError('');
+        setResponse(body);
+        setDone(true);
+      });
+  };
 
   if (!place || !questions.length) {
     return (
-      <div className='App antialiased font-sans bg-gray-50'>
+      <div className="App antialiased font-sans bg-gray-50">
         <Header>
           {' '}
           Hjälp Sverige! Vi behöver få in data för att indikera vart det krävs
@@ -59,29 +76,30 @@ function App () {
           svar om du behöver råd om vård.
         </Header>
 
-        <div className='formContainer mt-10 sm:mx-auto sm:w-full sm:max-w-md'>
-          <div className='questionContainer bg-white py-16 px-20 shadow sm:rounded-lg sm:px-24'>
-            <h2 className='text-2xl leading-8 my-8 font-semibold font-display text-gray-900 sm:text-3xl sm:leading-9 mb-5 mt-0'>
+        <div className="formContainer mt-10 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="questionContainer bg-white py-16 px-20 shadow sm:rounded-lg sm:px-24">
+            <h2 className="text-2xl leading-8 my-8 font-semibold font-display text-gray-900 sm:text-3xl sm:leading-9 mb-5 mt-0">
               Välj din kommun
             </h2>
             {error && <Error text={error} />}
             <Select
-              label='Kommuner'
-              placeholder='Kommuner'
-              options={places.map(p => {
+              label="Kommuner"
+              placeholder="Kommuner"
+              options={places.map((p) => {
                 return {
                   label: p,
-                  value: p
-                }
+                  value: p,
+                };
               })}
-              onChange={selected => setPlace(selected)}
+              onChange={(selected) => setPlace(selected)}
             />
             <Button
               onClick={() => {
                 if (place) {
-                  fetchQuestions()
+                  onCookieChange(place);
+                  fetchQuestions();
                 } else {
-                  setError('Ingen kommun vald')
+                  setError('Ingen kommun vald');
                 }
               }}
             >
@@ -90,34 +108,44 @@ function App () {
           </div>
         </div>
       </div>
-    )
+    );
   }
 
-  const onSubmit = values => {
+  const onSubmit = (values) => {
     answers.push({
       question: questions[questionIndex]._id,
       answer: values.answer,
-      place: place
-    })
+      place: place,
+    });
 
-    setAnswers(answers)
+    setAnswers(answers);
 
     if (questionIndex + 1 < questions.length) {
-      setQuestionIndex(questionIndex + 1)
+      setQuestionIndex(questionIndex + 1);
     } else {
-      submitAnswers(answers)
+      submitAnswers(answers);
     }
-  }
+  };
 
   return (
-    <div className='App h-auto'>
+    <div className="App h-auto">
       {done ? (
         <Done {...response} />
       ) : (
-        <Question {...questions[questionIndex]} onSubmit={onSubmit} />
+        <>
+          <Question {...questions[questionIndex]} onSubmit={onSubmit} />
+          <div className="mt-2">
+            <p>
+              Kommun: {place} -{' '}
+              <a onClick={onChangePlaceClick} href="#" className="hover:underline text-blue-700">
+                Ändra
+              </a>
+            </p>
+          </div>
+        </>
       )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
